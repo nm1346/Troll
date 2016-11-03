b3e7aa4f53c045193bdbc0967d24b71f22085c66
TigerWest <sj441888@naver.com>
2016-11-03 21:03:22 +0900

    searchnav제거

--- resources/page/index/indexctrl.js
+++ resources/page/index/indexctrl.js
@@ -1,3 +1,15 @@
 myApp.controller('indexCtrl', function($scope){
+	var video;
+	$scope.videoOn=true;
+	$scope.click=function(){
+		video.muted=true;
+		video.muted=false;
+	}
+	$scope.$watch("videoOn",function(newval,oldval){
+		if(newval==oldval)return;
+		newval="true"
+		video=document.getElementById("myVideo");
+
+	});
 
 });
