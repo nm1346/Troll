a8b708d07c72b76e7118e324c5fe97848f994cfb
TigerWest <sj441888@naver.com>
2016-11-04 16:15:11 +0900

    indexpage수정중

--- resources/page/index/indexctrl.js
+++ resources/page/index/indexctrl.js
@@ -1,15 +1,41 @@
-myApp.controller('indexCtrl', function($scope){
+myApp.controller('indexCtrl', function($scope,$timeout,$location){
 	var video;
 	$scope.videoOn=true;
-	$scope.click=function(){
-		video.muted=true;
-		video.muted=false;
-	}
+	$scope.mute=true;
 	$scope.$watch("videoOn",function(newval,oldval){
 		if(newval==oldval)return;
-		newval="true"
 		video=document.getElementById("myVideo");
-
+		if(newval==false){
+			$scope.mute=false;
+		}else{
+			$scope.mute=true;
+		}
 	});
+	$scope.$watch("mute",function(newval,oldval){
+		if(newval==oldval)return;
+		video=document.getElementById("myVideo");
+		if(video!=null){
+			if(newval==true){
+				video.muted=false;
+			}else{
+				video.muted=true;
+			}
+		}
+		
+	});
+	$scope.searchLayout={
+		loading:false
+	};
+	$scope.search = function (summonerName) {
+
+		if (summonerName == "" ||summonerName == null) {
+			Materialize.toast('소환사의 아이디를 입력해주세요.', 4000)
+			return;
+		}
+		$scope.searchLayout.loading=true;
+		$timeout(function(){
+			$location.path("/" +summonerName);
+		},1000);	
+	}
 
 });
