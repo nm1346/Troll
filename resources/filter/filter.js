1e722f3150d93509b092f25bddbd357dabaa8000
tawon2137 <ziezonn@naver.com>
2016-11-04 18:31:21 +0900

    디자인 수정중 16 11 04
    ㅁㄴㅇ

--- resources/filter/filter.js
+++ resources/filter/filter.js
@@ -4,3 +4,17 @@ myApp.filter('kdafilter',function () {
 		return kda.toFixed(2) + ':1' ;
 	}	
 });
+
+myApp.filter('victoryfilter',function () {
+	return function (boolean) {
+		if (boolean) {return '승리';
+		}else {return '패배';}
+	}	
+});
+
+myApp.filter('teamcolorfilter',function () {
+	return function (teamId) {
+		if (teamId == '100') {return 'blue';
+		}else {return 'red';}
+	}	
+});
