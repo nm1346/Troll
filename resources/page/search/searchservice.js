ae13127dfb414f0e6050aed35c16fad271803cda
tawon2137 <ziezonn@naver.com>
2016-11-16 21:16:09 +0900

    ㅇㅇㅇㅇ

--- resources/page/search/searchservice.js
+++ resources/page/search/searchservice.js
@@ -12,6 +12,26 @@ myApp.factory('summoner', function(){
 		set:function(data){
 			for (var member in searchdata) delete searchdata[member];
 			angular.extend(searchdata,data);
-		}
+		},
+		champscore:function (champarray) {
+		 			var mostarray = champarray;
+		 			var jumsu = [];
+		 			for (var i = 0; i < mostarray.length; i++) {
+		 				if (mostarray[i].deaths === 0) {
+		 					mostarray[i].deaths = 1
+		 				}
+		 				var kda = (mostarray[i].kills + mostarray[i].assists) /  mostarray[i].deaths;
+		 				var winlatescore = mostarray[i].winlate / mostarray[i].avgwinlate;
+		 				var scoreobject = {index : mostarray[i],
+		 					score:(winlatescore + (kda/ 2)).toFixed(2),
+		 					kda : kda.toFixed(2)}
+		 					jumsu.push(scoreobject);
+		 				}
+		 				console.log(jumsu);
+		 				jumsu.sort(function (a,b) {
+		 					return parseFloat(a.score) > parseFloat(b.score) ? -1 : parseFloat(a.score) < parseFloat(b.score) ? 1 : 0;  
+		 				});
+		 		return jumsu;
+		 } 
 	};
 })
