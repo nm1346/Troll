069af48c592471609ea51583b8ca04a55220bbea
nm1346 <dmswl19851@gmail.com>
2016-11-22 20:16:12 +0900

    수정
    찾았다

--- resources/page/search/searchservice.js
+++ resources/page/search/searchservice.js
@@ -1,40 +1,90 @@
 myApp.factory('SearchResource',function($resource,TrollRestUrl){
-    return $resource(TrollRestUrl+"user/:summonerName", {summonerName:"@summonerName"},{
-        get:{method:"GET"}
-    });
+   return $resource(TrollRestUrl+"user/:summonerName", {summonerName:"@summonerName"},{
+      get:{method:"GET"}
+   });
+});
+myApp.factory('itemResource',function($resource){
+   return $resource("http://ddragon.leagueoflegends.com/cdn/6.22.1/data/ko_KR/item.json", {},{
+      get:{method:"GET"}
+   });
+});
+myApp.factory('recentchampResource',function($resource, TrollRestUrl){
+   return $resource(TrollRestUrl+"recentchamp/:summonerId", {summonerId:"@summonerId"},{
+      get:{method:"GET"}
+   });
+});
+myApp.factory('SpellResource',function($resource){
+   return $resource("http://ddragon.leagueoflegends.com/cdn/6.22.1/data/ko_KR/summoner.json", {},{
+      get:{method:"GET"}
+   });
 });
 myApp.factory('summoner', function(){
-	var searchdata={};
-	return {
-		get:function(){
-			return searchdata;
-		},
-		set:function(data){
-			for (var member in searchdata) delete searchdata[member];
-			angular.extend(searchdata,data);
-		},
-		champscore:function (champarray) {
-		 			var mostarray = champarray;
-		 			var jumsu = [];
-		 			for (var i = 0; i < mostarray.length; i++) {
-		 				if (mostarray[i].deaths === 0) {
-		 					mostarray[i].deaths = 1
-		 				}
-		 				if(mostarray[i].avgwinlate === 0){
-		 					mostarray[i].avgwinlate = 50;
-		 				}
-		 				var kda = (mostarray[i].kills + mostarray[i].assists) /  mostarray[i].deaths;
-		 				var winlatescore = mostarray[i].winlate / mostarray[i].avgwinlate;
-		 				var scoreobject = {index : mostarray[i],
-		 					score:(winlatescore + (kda/ 2)).toFixed(2),
-		 					kda : kda.toFixed(2)}
-		 					jumsu.push(scoreobject);
-		 				}
-		 				console.log(jumsu);
-		 				jumsu.sort(function (a,b) {
-		 					return parseFloat(a.score) > parseFloat(b.score) ? -1 : parseFloat(a.score) < parseFloat(b.score) ? 1 : 0;
-		 				});
-		 		return jumsu;
-		 }
-	};
-})
+   var searchdata={};
+   var itemdata = {};
+   var spelldata = {};
+   return {
+      get:function(){
+         return searchdata;
+      },
+      set:function(data){
+         for (var member in searchdata) delete searchdata[member];
+            angular.extend(searchdata,data);
+      },
+      champscore:function (champarray) {
+         var mostarray = champarray;
+         var jumsu = [];
+         for (var i = 0; i < mostarray.length; i++) {
+            if (mostarray[i].deaths === 0) {
+               mostarray[i].deaths = 1
+            }
+            if(mostarray[i].avgwinlate === 0){
+               mostarray[i].avgwinlate = 50;
+            }
+            var kda = (mostarray[i].kills + mostarray[i].assists) /  mostarray[i].deaths;
+            var winlatescore = mostarray[i].winlate / mostarray[i].avgwinlate;
+            var scoreobject = {index : mostarray[i],
+               score:(winlatescore + (kda/ 2)).toFixed(2),
+               kda : kda.toFixed(2),
+               pickrank:i + 1}
+               jumsu.push(scoreobject);
+            }
+            jumsu.sort(function (a,b) {
+               return parseFloat(a.score) > parseFloat(b.score) ? -1 : parseFloat(a.score) < parseFloat(b.score) ? 1 : 0;
+            });
+            return jumsu;
+         },
+         getitem:function(){
+            return itemdata;
+         },
+         setitem:function(data){
+            for (var member in itemdata) delete itemdata[member];
+               angular.extend(itemdata,data);
+         },
+         getspell:function(){
+            return spelldata;
+         },
+         setspell:function(data){
+            for (var member in spelldata) delete spelldata[member];
+               angular.extend(spelldata,data);
+         },
+         addsummonerdata:function ($scope , summoner) {
+            var totaldata = summoner.get();
+            $scope.summonerdata = totaldata['summonerData'];
+            $scope.leaguedata = totaldata['leagueData'];
+            $scope.recentgame = totaldata['recentgamelist'];
+            if (totaldata['most'].length > 0) {
+               $scope.mostchamplist = summoner.champscore(totaldata['most']);
+            }
+
+            if (angular.isObject(totaldata['leagueData'])){
+               if (totaldata['leagueData'].entrylist[0].division === 'I') {
+                  $scope.tierurl = totaldata['leagueData'].tier.toLowerCase();
+               }else{
+                  $scope.tierurl = totaldata['leagueData'].tier.toLowerCase() +'_'+totaldata['leagueData'].entrylist[0].division.toLowerCase();
+               }
+            }else{
+               $scope.tierurl = "unlanked";
+            }
+         }
+      };
+   })
--- resources/page/search/summoner/summonerdirective.js
+++ resources/page/search/summoner/summonerdirective.js
@@ -1,58 +1,133 @@
-
-myApp.directive('summonerData', function(matchResource,matchData,SearchResource,summoner,$routeParams,BoardData){
-	return {
-		 scope: {}, // {} = isolate, true = child, false/undefined = no change
-		 controller: function($scope, $element, $attrs, $transclude) {
-			/*$scope.$emit('searchPageStart',{});*/
-			$scope.match= function(matchId){
+myApp.directive('summonerData', function(matchResource,matchData,SearchResource,summoner,$routeParams,BoardData,$location,$http,itemResource,SpellResource,
+   recentchampResource){
+   return {
+       scope: {}, // {} = isolate, true = child, false/undefined = no change
+       controller: function($scope, $element, $attrs, $transclude) {
+       	  $scope.match= function(matchId){
 				//alert(matchId);
 				//match로 전달 후 페이지 시작
 	    		$scope.$emit("CoverOn",{});
 	    		$scope.$emit("loadingOn",{});
 	            matchResource.get({matchId : matchId}).$promise.then(function(data){
 	            	matchData.setmatch(data);
+	            	matchData.setsummoner(summoner.get());
 	            	$scope.$emit("searchViewChange",3);
 				},function(error){
 					$scope.$emit("loadingOff",{});
 				});
 			};
+          /*$scope.$emit('searchPageStart',{});*/
+          $scope.$emit("loadingOn",{});
+          $scope.$emit('searchPageStart', {loading : true , error : false});
+          itemResource.get({}).$promise.then(function (data) {
+             summoner.setitem(data);
+             $scope.itemdata = summoner.getitem();
+          },function (err) {
+             console.log('item 불러오기 err :  ',err)
+          })
+          SpellResource.get({}).$promise.then(function (data) {
+             summoner.setspell(data);
+             $scope.spelldata = summoner.getspell();
+             console.log($scope.spelldata);
+          },function (err) {
+             console.log('spell 불러오기 err :  ',err)
+          });
+          if (Object.keys(summoner.get()).length === 0) {
+             SearchResource.get({summonerName : $routeParams.summonerName}).$promise.then(function (data) {
+                if (Boolean(Number(data.success))){
+                   summoner.set(data);
+                   summoner.addsummonerdata($scope, summoner);
+                   $scope.$emit('searchPageSuccess', {loading : false , error : false});
+                   $scope.$emit("loadingOff",{});
+                }else{
+                   $scope.$emit('searchPageError', {errorCode : data.errorcode , errorMessage : data.errormsg});
+                   $scope.$emit("loadingOff",{});
+                }
+             },function (error) {
+                console.log('에러',error);
+             });
+          }else{
+             if ($routeParams.summonerName !== summoner.get().summonerData.name) {
+                SearchResource.get({summonerName : $routeParams.summonerName}).$promise.then(function (data) {
+                   if (Boolean(Number(data.success))){
+                      summoner.set(data);
+                      summoner.addsummonerdata($scope, summoner);
+                      $scope.$emit('searchPageSuccess', {loading : false , error : false});
+                      $scope.$emit("loadingOff",{});
+                   }else{
+                      $scope.$emit('searchPageError', {errorCode : data.errorcode , errorMessage : data.errormsg});
+                      $scope.$emit("loadingOff",{});
+                   }
+                },function (error) {
+                   console.log('에러',error);
+                });
+             }else{
+                summoner.addsummonerdata($scope, summoner);
+                $scope.$emit('searchPageSuccess', {loading : false , error : false});
+                $scope.$emit("loadingOff",{});
+             }
+          }
+          $scope.mostindex = "";
+          $scope.mostindexreset = function () {
+             $scope.mostindex = "";
+          }
+          $scope.researchsummoner = function (summonerName) {
+             $location.path('/'+summonerName);
+          }
+       },
+      restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+      templateUrl: '/resources/page/search/summoner/summonerdata.html',
+      link: function($scope, iElm, iAttrs, controller) {
+         $('#recentchampmodal').modal({
+            dismissible:true,
+            opacity:0,
+            ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+               var overlay = $('.modal-overlay');
+            // remove it
+            overlay.detach();
+         },
+      });
 
-			$scope.$emit("loadingOn",{});
-			$scope.$emit('searchPageStart', {loading : true , error : false});
-			SearchResource.get({summonerName : $routeParams.summonerName}).$promise.then(function (data) {
-				if (Boolean(Number(data.success))){
-					summoner.set(data);
-					matchData.setsummoner(data);
-					console.log(data)
-					$scope.$emit('searchPageSuccess', {loading : false , error : false});
-					$scope.$emit("loadingOff",{});
-				}else{
-					$scope.$emit('searchPageError', {errorCode : data.errorcode , errorMessage : data.errormsg});
-					$scope.$emit("loadingOff",{});
-				}
-			},function (error) {
-				console.log('에러',error);
-			});
-			$scope.$on('pageonview',function (data) {
-				var totaldata = summoner.get();
-				$scope.summonerdata = totaldata['summonerData'];
-				$scope.leaguedata = totaldata['leagueData'];
-				$scope.recentgame = totaldata['recentgamelist'];
-				if (angular.isObject(totaldata['leagueData'])){
-					$scope.tierurl = totaldata['leagueData'].tier.toLowerCase();
-					$scope.divisionurl = totaldata['leagueData'].entrylist[0].division.toLowerCase();
-				}else{
-					$scope.unlanked = totaldata['leagueData'];
-				}
-			});
-			$scope.avgcs =  function (stats) {
-				console.log(stats.minionsKilled,stats.timePlayed);
-				return (stats.minionsKilled / (stats.timePlayed / 60)).toFixed(2);
-			}
-		},
-		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-		templateUrl: '/resources/page/search/summoner/summonerdata.html',
-		link: function($scope, iElm, iAttrs, controller) {
-		}
-	};
+         $scope.getrecentchamp = function (summonerId) {
+            console.log(summonerId);
+            recentchampResource.get({summonerId : summonerId}).$promise.then(function (data) {
+               console.log(data);
+               for (var i = 0; i < data['champlist'].length; i++) {
+                  data['champlist'][i].drag = true
+               }
+               $scope.recentchamplist = data['champlist'];
+               console.log($scope.recentchamplist);
+               $('#recentchampmodal').modal('open');
+            },function (error) {   
+               console.log(error);
+            });
+         }   
+         $scope.startCallback = function(event, ui, title) {
+            console.log('You started draggin: ' + title.title);
+            $scope.draggedTitle = title.title;
+         };
+
+         $scope.stopCallback = function(event, ui) {
+            console.log('Why did you stop draggin me?');
+         };
+
+         $scope.dragCallback = function(event, ui) {
+            console.log('hey, look I`m flying');
+         };
+
+         $scope.dropCallback = function(event, ui) {
+            console.log('hey, you dumped me :-(' , $scope.draggedTitle);
+         };
+
+         $scope.overCallback = function(event, ui) {
+            console.log('Look, I`m over you');
+         };
+
+         $scope.outCallback = function(event, ui) {
+            console.log('I`m not, hehe');
+         };
+      }
+   };
 });
+
+
